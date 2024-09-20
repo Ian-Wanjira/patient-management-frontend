85b247c35ce40c1581271f22405d6bce0a60a082
Ian-Wanjira <ianwanjira4@gmail.com>
2024-09-20 05:12:17 -0500

    feat: create PatientData interface for defining default form values

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,30 @@
 declare type Gender = 'male' | 'female' | 'other';
 
-declare interface userParams {
+declare interface UserParams {
   name: string;
   email: string;
   phone: string;
+}
+
+declare interface PatientData extends UserParams {
+  dateOfBirth: Date;
+  gender: string;
+  address: string;
+  occupation: string;
+  emergencyContactName: string;
+  emergencyContactPhone: string;
+  primaryCarePhyician: string;
+  insuranceProvider: string;
+  insurancePolicyNumber: string;
+  allergies: string;
+  currentMedicaitons: string;
+  familyMedicalHistory: string;
+  pastMedicalHistory: string;
+  identificationType: string;
+  indentificationNumber: string;
+  identificationDocument: File[];
+  treatmentConsent: boolean;
+  disclosureConsent: boolean;
+  privacyConsent: boolean;
+  password: string;
 }

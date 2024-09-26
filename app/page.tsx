1948cd0a50710c1e39347c0da420972a60b9f50c
Ian-Wanjira <ianwanjira4@gmail.com>
2024-09-26 08:05:27 -0500

    style: change the overflow-y property to auto

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ const Home = () => {
     <>
       {isRegisterForm ? (
         <div className="flex h-screen max-h-screen">
-          <section className="remove-scrollbar container">
+          <section className="remove-scrollbar container overflow-y-auto">
             <div className="sub-container max-w-[800px] flex-1 flex-col py-10">
               <Image
                 src="/assets/icons/logo-full.svg"
@@ -50,7 +50,7 @@ const Home = () => {
         </div>
       ) : (
         <div className="flex h-screen max-h-screen">
-          <section className="remove-scrollbar container my-auto">
+          <section className="remove-scrollbar container overflow-y-auto">
             <div className="sub-container max-w-[496px]">
               <Image
                 src="/assets/icons/logo-full.svg"

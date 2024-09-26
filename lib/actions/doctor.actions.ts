07338ec40a21d15fcfb5bb837b49df74c30dde6b
Ian-Wanjira <ianwanjira4@gmail.com>
2024-09-26 08:21:07 -0500

    feat: create getDoctors, getDoctor functions for retrieving a doctors list and retrieving and individual doctor from an api

---
+++ lib/actions/doctor.actions.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
+
+export const getDoctors = async () => {
+  try {
+    const response = await axiosInstance.get('/api/doctor/list/');
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getDoctor = async (id: string) => {
+  try {
+    const response = await axiosInstance(`/api/doctor/${id}/`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};

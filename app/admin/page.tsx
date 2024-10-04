0122b0347f08348755f0afc76eac418f0319ea09
Ian-Wanjira <ianwanjira4@gmail.com>
2024-10-04 02:31:27 -0500

    feat: create admin component for rendering patient appointments from api

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,17 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
 
-const page = () => {
+import StatCard from '@/components/StatCard';
+import { columns } from '@/components/table/columns';
+import { DataTable } from '@/components/table/DataTable';
+import { getAppointments } from '@/lib/actions/appointment.actions';
+
+const page = async () => {
+  const { appointments, counts } = (await getAppointments()) as {
+    appointments: Appointment[];
+    counts: AppointmentCounts;
+  };
+
   return (
     <div className="mx-auto flex max-w-7xl flex-col space-y-14">
       <header className="admin-header">
@@ -18,6 +27,38 @@ const page = () => {
 
         <p className="text-16-semibold">Admin Dashboard</p>
       </header>
+
+      <main className="admin-main">
+        <section className="w-full space-y-4">
+          <h1 className="header">Welcome 👋</h1>
+          <p className="text-dark-700">
+            Start the day with managing new appointments
+          </p>
+        </section>
+
+        <section className="admin-stat">
+          <StatCard
+            type="appointments"
+            count={counts.scheduled}
+            label="Scheduled appointments"
+            icon="/assets/icons/appointments.svg"
+          />
+          <StatCard
+            type="pending"
+            count={counts.pending}
+            label="Pending appointments"
+            icon="/assets/icons/pending.svg"
+          />
+          <StatCard
+            type="cancelled"
+            count={counts.cancelled}
+            label="Cancelled appointments"
+            icon="/assets/icons/cancelled.svg"
+          />
+        </section>
+
+        <DataTable columns={columns} data={appointments} />
+      </main>
     </div>
   );
 };

298770191d2dbc5fb257827f454f947891810296
Ian-Wanjira <ianwanjira4@gmail.com>
2024-10-04 02:22:49 -0500

    feat: add StatusIcon object for different types of icons based on type

--- constants/index.ts
+++ constants/index.ts
@@ -93,3 +93,9 @@ export const Doctors = [
     name: 'Hardik Sharma',
   },
 ];
+
+export const StatusIcon = {
+  scheduled: '/assets/icons/check.svg',
+  pending: '/assets/icons/pending.svg',
+  cancelled: '/assets/icons/cancelled.svg',
+};
